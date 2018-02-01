0c4321b75aba142bf01d934c22bf78017942e7c8
sainadh007 <15co216.sainadh98@nitk.edu.in>
2018-02-01 20:22:34 +0530

    Add files via upload

---
+++ ita/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Main():
+    return render_template("homepage.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
