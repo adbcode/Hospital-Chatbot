13cae436b565035b44b004a8679803fbaf3aa2a8
bharath113 <bandarubharath7@gmail.com>
2018-03-09 14:18:47 +0530

    Add files via upload

--- ita/app.py
+++ ita/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import *
+import json,requests,sys
+import apiai
 
 app = Flask(__name__)
+
+CLIENT_ACCESS_TOKEN = 'c0dce7652e6b4a16b3e818ff84b78373'
+
+ai= apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 @app.route('/')
 def Main():
@@ -8,4 +14,5 @@ def Main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,ssl_context='adhoc')
+	
