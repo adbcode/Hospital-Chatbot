48e64065a28eaf15f0a29a256f69a4b9b51d10a3
adbcode <9251629+adbcode@users.noreply.github.com>
2018-04-17 11:35:25 +0530

    Add files via upload

--- hosbot/app2.py
+++ hosbot/app2.py
@@ -4,6 +4,7 @@ from flask import *
 import json,requests,sys
 import apiai
 import sqlite3
+import datetime
 
 app = Flask(__name__)
 
@@ -164,6 +165,69 @@ def makeWebhookResult(req):
 			speech = "New patient profile created. Your patient id is: " + str(data[0]) + "."
 		else: 
 			speech = "Profile already exists. Your patient id is: " + str(data[0][0]) + "."
+		print("Response:")
+		print(speech)
+		conn.commit()
+		conn.close()
+		return {
+			"speech" : speech,
+			"displayText": speech,
+			"source": "Healthmaster"
+		}
+		
+	# Creating a new patient profile
+	if stri=="appointment.book":
+		conn = sqlite3.connect('hospital.db')
+		result = req.get("result")
+		parameters = result.get("parameters")
+		pid = str(parameters.get("pid"))
+		time = str(parameters.get("time"))
+		date = str(parameters.get("date"))
+		did = str(parameters.get("did"))
+		purpose = str(parameters.get("purpose"))
+		location = str(parameters.get("locations"))
+		
+		dictionary = {"mangalore" : 1, "panaji" : 2, "jaipur" : 3, "salem" : 4, "vijayawada" : 5}
+		
+		if location in dictionary.keys():
+			hid = str(dictionary[location.lower()])
+			cursor = conn.execute("SELECT * FROM DOCTOR WHERE DID = " + did + " AND HID = " + hid)
+			data = cursor.fetchall()
+			
+			if len(data) != 0:
+				cursor = conn.execute("SELECT * FROM PATIENT WHERE PID = " + pid)
+				data = cursor.fetchall()
+				
+				if len(data) != 0:
+					DateTime = date + " " + time
+					cursor = conn.execute("SELECT * FROM APPOINTMENT WHERE DID = " + did + " AND ADATETIME = \"" + DateTime + "\" AND HID = " + HID)
+					data = cursor.fetchall()
+					
+					if len(data) != 0:
+						if location in data:
+							speech = "Your appointment is already booked for the given time and doctor."
+						else:
+							speech = "The requested doctor already has appointment in the given time. Please book for another time."
+					
+					else:
+						m = datetime.datetime.strptime(date,'%Y-%m-%d').strftime('%A')
+						cursor = conn.execute("SELECT TIME FROM AVAILABLE WHERE DID = " + did + " AND WEEKDAY = " + m)
+						time_variable = cursor.fetchall()[0]
+						time1, time2 = time_variable.split("-")
+						timeA = datetime.datetime.strptime(time1, "%H:%M:%S")
+						timeB = datetime.datetime.strptime(time2, "%H:%M:%S")
+						timeC = datetime.datetime.strptime(time, "%H:%M:%S")
+						if timeC >timeA and timeC < timeB:
+							cursor = conn.execute("INSERT INTO APPOINTMENT(PID, DID, HID, PURPOSE, ADATETIME, AFEE) VALUES(" + pid + ", " + did + ", " + hid + ", \"" + purpose + "\", \"" + DateTime + "\", 0)")
+						else:
+							speech = "The requested doctor is unavailable at the requested time. Please book for another time."
+				else:
+					speech="The patient does not exist in the records. Please try again or register before booking an appointment."
+			else:
+				speech = "The doctor does not exist in the records and/or the doctor does not work in the given hospital. Please try again."
+		else:
+			speech = "The location does not exist in the records. Please try again."
+						
 		print("Response:")
 		print(speech)
 		conn.commit()
